f260710239ae6e9b9104154ac013a0acb07c7c93
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-09-23 17:48:48 +1000

    updated review validation and addeded reviews freature to campground route

--- schemas.js
+++ schemas.js
@@ -10,10 +10,10 @@ module.exports.campgroundSchema = Joi.object({
     deleteImages: Joi.array()
 });
 
-// module.exports.reviewSchema = Joi.object({
-//     review: Joi.object({
-//         rating: Joi.number().required().min(1).max(5),
-//         body: Joi.string().required()
-//     }).required()
-// })
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
 
