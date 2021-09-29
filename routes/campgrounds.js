226b6dd5c0d91ef1743dd2a13e7b1aed329a5b53
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-09-29 17:57:04 +1000

    added flash for successe and started flash for error

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,7 +28,7 @@ router.get('/new', (req, res) => {
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    req.flash('success', 'New CG Yaay');
+    req.flash('success', 'Successfully created a new campground');
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
@@ -45,12 +45,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
     res.redirect('/campgrounds');
 }))
 
