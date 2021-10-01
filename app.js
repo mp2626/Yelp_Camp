53c57a88bb59a7d6ca33508ad70cab473af5ab17
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-10-02 09:20:34 +1000

    start the setup of authentication

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/expressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -47,11 +50,24 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
+
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'mpe@mp.com', username: 'MPerrin' });
+    const newUser = await User.register(user, 'password');
+    res.send(newUser);
+});
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
